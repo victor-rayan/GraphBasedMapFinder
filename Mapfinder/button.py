aa09fc22f78f386683398dd6848135ed922380f2
Mateus Moreira <mateuuusss@gmail.com>
2022-07-10 15:48:47 -0300

    Adicionado efeito sonoro menu

--- Mapfinder/button.py
+++ Mapfinder/button.py
@@ -4,33 +4,45 @@ import pygame
 Classe Button instancia um objeto do tipo button, método draw desenha na tela utilizando surface como parametro.
 '''
 
+
 class ButtonScreen():
 
-	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
-		self.image = image
-		self.x_pos = pos[0]
-		self.y_pos = pos[1]
-		self.font = font
-		self.base_color, self.hovering_color = base_color, hovering_color
-		self.text_input = text_input
-		self.text = self.font.render(self.text_input, True, self.base_color)
-		if self.image is None:
-			self.image = self.text
-		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color, hover_sfx=None, isHover=False):
+        self.image = image
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.font = font
+        self.base_color, self.hovering_color = base_color, hovering_color
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+        self.hover_sfx = hover_sfx
+        self.isHover = isHover
 
-	def update(self, screen):
-		if self.image is not None:
-			screen.blit(self.image, self.rect)
-		screen.blit(self.text, self.text_rect)
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
 
-	def checkForInput(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			return True
-		return False
+    def checkForInput(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            return True
+        return False
 
-	def changeColor(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			self.text = self.font.render(self.text_input, True, self.hovering_color)
-		else:
-			self.text = self.font.render(self.text_input, True, self.base_color)
+    def changeColor(self, position):
+        if self.checkForInput(position):
+            self.text = self.font.render(
+                self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(
+                self.text_input, True, self.base_color)
+
+    def playOnHover(self, position):
+        if self.checkForInput(position) and (self.hover_sfx != None) and (self.isHover == False):
+            self.isHover = True
+            self.hover_sfx.play()
+        elif not self.checkForInput(position):
+            self.isHover = False
--- Mapfinder/screens.py
+++ Mapfinder/screens.py
@@ -9,7 +9,7 @@ import const as c
 RES = c.WIDTH, c.HEIGHT
 pygame.init()
 game_surface = pygame.Surface(RES)
-surface = pg.display.set_mode((c.WIDTH + 405, c.HEIGHT ))
+surface = pg.display.set_mode((c.WIDTH + 405, c.HEIGHT))
 
 
 bg_game = pg.image.load('../assets/backgroundgta.png').convert()
@@ -29,10 +29,23 @@ jetpack_img = pg.transform.scale(jetpack_img, (80, 80))
 jetpack_rect = jetpack_img.get_rect()
 jetpack_rect.center = WIDTH + 250, 350
 
+hoverButton = pg.mixer.Sound(path.join(dir_icon, 'hoverButton.mp3'))
 
-def get_font(size): 
+
+def get_font(size):
     return pygame.font.Font(path.join(dir_icon, 'font.ttf'), size)
+
+
 path.join(dir_icon, 'Play Rect.png')
+
+PLAY_BUTTON = ButtonScreen(image=pygame.image.load(path.join(dir_icon, 'Quit Rect.png')), pos=(WIDTH + 150, 200),
+                           text_input="CARRO", font=get_font(20), base_color="White", hovering_color="Blue", hover_sfx=hoverButton)
+JETTPACK = ButtonScreen(image=pygame.image.load(path.join(dir_icon, 'Quit Rect.png')), pos=(WIDTH + 150, 350),
+                        text_input="JETPACK", font=get_font(20), base_color="White", hovering_color="Blue", hover_sfx=hoverButton)
+TERRESTRE = ButtonScreen(image=pygame.image.load(path.join(dir_icon, 'Quit Rect.png')), pos=(WIDTH + 150, 500),
+                         text_input="VER DJKISTRA", font=get_font(20), base_color="White", hovering_color="Blue", hover_sfx=hoverButton)
+QUIT_BUTTON = ButtonScreen(image=pygame.image.load(path.join(dir_icon, 'Quit Rect.png')), pos=(WIDTH + 150, 650),
+                           text_input="SAIR", font=get_font(20), base_color="White", hovering_color="Blue", hover_sfx=hoverButton)
 
 def startgame():
     while True:
@@ -40,24 +53,17 @@ def startgame():
         surface.blit(jetpack_img, jetpack_rect)
         surface.blit(game_surface, (0, 0))
         game_surface.blit(bg_game, (100, 60))
-        
+
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        PLAY_BUTTON = ButtonScreen(image=pygame.image.load(path.join(dir_icon, 'Quit Rect.png')), pos=(WIDTH + 150, 200), 
-                            text_input="CARRO", font=get_font(20), base_color="White", hovering_color="Blue")
-        JETTPACK = ButtonScreen(image=pygame.image.load(path.join(dir_icon, 'Quit Rect.png')), pos=(WIDTH + 150, 350), 
-                            text_input="JETPACK", font=get_font(20), base_color="White", hovering_color="Blue")
-        TERRESTRE = ButtonScreen(image=pygame.image.load(path.join(dir_icon, 'Quit Rect.png')), pos=(WIDTH + 150, 500), 
-                            text_input="VER DJKISTRA", font=get_font(20), base_color="White", hovering_color="Blue")                                         
-        QUIT_BUTTON = ButtonScreen(image=pygame.image.load(path.join(dir_icon, 'Quit Rect.png')), pos=(WIDTH + 150, 650), 
-                            text_input="SAIR", font=get_font(20), base_color="White", hovering_color="Blue")
+        surface.blit(text_font.render('FindMap', True,
+                     pygame.Color('cyan'), True), (WIDTH + 10, 30))
 
-        surface.blit(text_font.render('FindMap', True, pygame.Color('cyan'), True), (WIDTH + 10, 30))
-        
         for button in [PLAY_BUTTON, JETTPACK, TERRESTRE, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
+            button.playOnHover(MENU_MOUSE_POS)
             button.update(surface)
-        
+            
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -69,11 +75,8 @@ def startgame():
                     pygame.quit()
                     sys.exit()
 
-                   
-
         pygame.display.update()
-    
-           
+
 
 if __name__ == '__main__':
-    startgame()     
+    startgame()
